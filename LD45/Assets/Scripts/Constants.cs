d28cd055b1fcf2a0f044299fad10eab77b54a3f6
GreenCalx <romain.schoenberger@gmail.com>
2019-10-05 23:19:16 +0200

    Broken bridge at the void cross @GreenCalx

--- LD45/Assets/Scripts/Constants.cs
+++ LD45/Assets/Scripts/Constants.cs
@@ -7,4 +7,5 @@ public class Constants
     public const string WORLD_GO_ID = "WORLD";
     public const string PLAYER_GO_ID = "Player";
 
+    public const string BROKEN_BRIDGE_MAP_SUBFORM_GO_ID = "Tilemap_subform0";
 }
