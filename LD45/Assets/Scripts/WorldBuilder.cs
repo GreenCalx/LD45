29fd63d688a1dd80ac32dde44fb30f0b92bf934a
GreenCalx <romain.schoenberger@gmail.com>
2019-10-05 13:31:44 +0200

    Game buildo start @GreenCalx

--- LD45/Assets/Scripts/WorldBuilder.cs
+++ LD45/Assets/Scripts/WorldBuilder.cs
@@ -4,13 +4,27 @@ using UnityEngine;
 
 public class WorldBuilder : MonoBehaviour
 {
+    public GameObject gridWorld0GO;
+    public GameObject gridWorld1GO;
+    public GameObject gridWorld2GO;
 
+    private GameObject currentGridWorld;
 
     public void build( uint major_state, uint minor_state)
     {
 
         // MAJOR STATE
-
+        switch (major_state)
+        {
+            case 1:
+                currentGridWorld = gridWorld1GO;
+                break;
+            case 2:
+                currentGridWorld = gridWorld2GO;
+                break;
+            default:
+                break;
+        }
 
         // MINOR STATE
 
@@ -25,7 +39,9 @@ public class WorldBuilder : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (!!gridWorld0GO)
+            currentGridWorld = gridWorld0GO;
+
     }
 
     // Update is called once per frame
