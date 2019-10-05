aa6bb545c3730d64807089b7f4367d4f668b6270
GreenCalx <romain.schoenberger@gmail.com>
2019-10-05 12:07:30 +0200

    World @GreenCalx

---
+++ LD45/Assets/Scripts/World.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class World : MonoBehaviour
+{
+    private WorldBuilder worldBuilder;
+
+    // Main Layers modifications
+    private uint world_state { get; set; }
+
+    // Secondary ( Graphical ) layers modifications
+    private uint world_substate { get; set; }
+
+    public void upgrade( bool isMajorUpgrade )
+    {
+        if (isMajorUpgrade) world_state++;
+        else world_substate++;
+
+        updateWorld();
+    }
+
+    public void updateWorld()
+    {
+        if (!!worldBuilder)
+            worldBuilder.build(world_state, world_substate);
+    }
+
+    /// ------------ UNITY ------------
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        worldBuilder = this.GetComponent<WorldBuilder>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+}
---
+++ LD45/Assets/Scripts/WorldBuilder.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldBuilder : MonoBehaviour
+{
+
+
+    public void build( uint major_state, uint minor_state)
+    {
+
+        // MAJOR STATE
+
+
+        // MINOR STATE
+
+
+    }
+
+
+
+
+    /// ------------ UNITY ------------
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
