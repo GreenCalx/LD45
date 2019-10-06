414e1806961665a90cc65b645e0f57cb2ea406a8
GreenCalx <romain.schoenberger@gmail.com>
2019-10-06 20:21:52 +0200

    Broken bridge 2 @GreenCalx

---
+++ LD45/Assets/Scripts/SpawnUpgradeAtLocation.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnUpgradeAtLocation : MonoBehaviour
+{
+    public GameObject location;
+    public GameObject world_upgrade;
+
+    private bool hasBeenTriggered = false;
+
+    public void trigger()
+    {
+        if (!hasBeenTriggered)
+        {
+            GameObject newUpgradeGO = Instantiate(world_upgrade);
+            newUpgradeGO.transform.position = location.transform.position;
+            hasBeenTriggered = true;
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        hasBeenTriggered = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        PlayerController pc = collision.GetComponent<PlayerController>();
+        if (!!pc)
+        {
+            trigger();
+        }
+    }
+}
