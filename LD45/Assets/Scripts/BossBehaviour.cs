ec375c3ca3bb59287458481b0217490b97391b34
GreenCalx <romain.schoenberger@gmail.com>
2019-10-07 18:46:05 +0200

    boss wip @GreenCalx

--- LD45/Assets/Scripts/BossBehaviour.cs
+++ LD45/Assets/Scripts/BossBehaviour.cs
@@ -6,22 +6,41 @@ public class BossBehaviour : MonoBehaviour
 {
     public bool casting_spell_1 = false;
     public bool casting_spell_2 = false;
+    public bool is_teleporting = false;
+    public bool is_dashToPlyer = false;
+
+    public bool bossPulled = false;
+
+    // SPELL 1
+    int shot_n_missiles = 2;
+    public GameObject missile_spell1_GO;
+    private List<GameObject> firedProjectiles;
 
     public const float waitTime = 3f;
     public float elapsedTime = 0f;
+
+    // PLAYER
+    private GameObject playerGO;
 
     // Start is called before the first frame update
     void Start()
     {
         elapsedTime = 0f;
+        bossPulled = false;
+        firedProjectiles = new List<GameObject>(shot_n_missiles);
+        playerGO = GameObject.Find(Constants.PLAYER_GO_ID);
     }
 
     // Update is called once per frame
     void Update()
     {
-        elapsedTime += Time.deltaTime;
-        if (elapsedTime > waitTime)
-            doStuff();
+        if (bossPulled)
+        {
+            elapsedTime += Time.deltaTime;
+            if (elapsedTime > waitTime)
+                doStuff();
+
+        }//! boss pulled
 
         // animator
         updateAnimator();
@@ -52,21 +71,47 @@ public class BossBehaviour : MonoBehaviour
     public void castSpell_1()
     {
         casting_spell_1 = true;
+        
+        for (int i = 0; i < shot_n_missiles; i++ )
+        {
+            GameObject newMissile = Instantiate<GameObject>(missile_spell1_GO);
+            firedProjectiles.Add(newMissile);
+            float deviation_rate = ( 0.1f * i );
+            newMissile.transform.position = transform.position;
+
+
+            
+        }
+
+
+    }
+
+    public void activate()
+    {
+        bossPulled = true;
+    }
+
+    public void clearSpells()
+    {
+        // clear spell 1
+        foreach (GameObject missile in firedProjectiles)
+        { Destroy(missile); }
     }
 
     public void castSpell_2()
+
     {
         casting_spell_2 = true;
     }
 
     public void teleport()
     {
-
+        is_teleporting = true;
     }
 
     public void dashToPlayer()
     {
-        
+        is_dashToPlyer = true;
     }
 
     public void updateAnimator()
---
+++ LD45/Assets/Scripts/BossBolt.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossBolt : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void fireBolt( Transform target, float deviation )
+    {
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        PlayerController pc = collision.GetComponent<PlayerController>();
+        if (!!pc)
+        {
+            pc.hp -= 1;
+            Destroy(this.gameObject);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        PlayerController pc =  collision.gameObject.GetComponent<PlayerController>();
+        if (!!pc)
+        {
+            pc.hp -= 1;
+        }
+        Destroy(this.gameObject);
+    }
+
+}
