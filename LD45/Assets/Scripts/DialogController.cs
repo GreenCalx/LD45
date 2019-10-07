2741e55cfb2060f6a352566b84e083bb6d60aa06
GreenCalx <romain.schoenberger@gmail.com>
2019-10-07 22:13:29 +0200

    Comma @GreenCalx

--- LD45/Assets/Scripts/DialogController.cs
+++ LD45/Assets/Scripts/DialogController.cs
@@ -27,7 +27,7 @@ public class DialogController : MonoBehaviour
         "You dried the lake!\nYou can now walk back to the labyrinth and claim this universe !",
 
         // WORLD 4
-        "You found some lifeforce ! \nYou fully recovered !\nUse LEFT ALT to smite your foes."
+        "You found some lifeforce ! \nYou fully recovered !\nUse LEFT ALT to smite your foes.",
         "You killed Zombologg !\nYou successfully claimed this universe !"
     };
 
