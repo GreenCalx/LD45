83b33073031ca202c9c69d6567566a34eab54e10
GreenCalx <romain.schoenberger@gmail.com>
2019-10-07 22:12:06 +0200

    som more text @GreenCalx

--- LD45/Assets/Scripts/DialogController.cs
+++ LD45/Assets/Scripts/DialogController.cs
@@ -9,8 +9,26 @@ public class DialogController : MonoBehaviour
     private bool dialog_is_on = false;
     private static readonly string[] lines =
     {
-        "Hello there, newborn.\nTake CONTROL and check that shiny square out !",
-        "You gained control !\nUse WASD to move around."
+        // WORLD 0
+        "Hello there, newborn.\nTake CONTROL and check that primary essence out !", // done
+        "You gained control !\nUse WASD/Arrows to move around.", // done
+        
+        // WORLD 1
+        "This essence builds your universe.\nMany more can be collected.",
+        "You found some lifeforce ! \n You are a Spirit now.\nOnly the deep void can stop you!",
+        
+        // WORLD 2
+        "The next lifeforce lies at the heart of the labyrinthe. \nDon't forget to collect essences.",
+        "You found some lifeforce ! \nYou are a Slime now.\nJust a weak physical creature with a physical body.",
+        "The next lifeforce lies across the broken bridge. \nDon't forget to collect essences.",
+
+        // WORLD 3
+        "You found some lifeforce ! \nYou are a Tongueling now.\nUse your tongue with SHIFT to lick around.",
+        "You dried the lake!\nYou can now walk back to the labyrinth and claim this universe !",
+
+        // WORLD 4
+        "You found some lifeforce ! \nYou fully recovered !\nUse LEFT ALT to smite your foes."
+        "You killed Zombologg !\nYou successfully claimed this universe !"
     };
 
     public  float  dialog_min_duration  = 2.0f;
